d27df5421477baef91d349fd0ba376b3cba07ea5
root <root@localhost.localdomain>
2025-05-22 03:20:45 +0200

    try_corrige and clean

--- app.py
+++ app.py
@@ -4,28 +4,27 @@ import pandas as pd
 import duckdb
 import io
 
-#from duckdb.duckdb import query
-#from sepolgen.classperms import result
+# from duckdb.duckdb import query
+# from sepolgen.classperms import result
 
-csv='''
+csv = """
 bevarage, price
 orangejuice, 2.5
 expresso, 2
 tea,3
-'''
-beverages= pd.read_csv(io.StringIO(csv))
+"""
+beverages = pd.read_csv(io.StringIO(csv))
 
-csv2='''
+csv2 = """
 food_item, food_price
 cookie juice, 2.5
 chocolatine, 2
 muffin,3
-'''
+"""
 food_items = pd.read_csv(io.StringIO(csv2))
 
 answer = """SELECT * FROM beverages CROSS JOIN food_items"""
 solution = duckdb.query(answer).df()
-
 
 
 with st.sidebar:
@@ -35,9 +34,9 @@ with st.sidebar:
         index=None,
         placeholder="Select a theme ...",
     )
-    st.write('You dselected: ', option)
+    st.write("You dselected: ", option)
 
-st.header('entrer votre code:')
+st.header("entrer votre code:")
 query = st.text_area(label="votre code sql ici", key="user_input")
 
 if query:
@@ -52,20 +51,22 @@ if query:
 
     n_lines_difference = result.shape[0] - solution.shape[0]
     if n_lines_difference != 0:
-        st.write( f"result has a '{n_lines_difference}' lines difference with the solution")
+        st.write(
+            f"result has a '{n_lines_difference}' lines difference with the solution"
+        )
 
 
 tab1, tab2 = st.tabs(["tables", "solution"])
 
 with tab1:
     # selection et requête
-     # sql_query = st.text_area(label="entrez votre input")
+    # sql_query = st.text_area(label="entrez votre input")
     # result = duckdb.query(sql_query).df()
     # st.write(f"vous avez entré la requête:{sql_query}")
     # st.dataframe(result)
 
     st.write("beverages")
-    sql_query1= """SELECT * FROM beverages"""
+    sql_query1 = """SELECT * FROM beverages"""
     result1 = duckdb.query(sql_query1).df()
     st.dataframe(result1)
 
@@ -85,13 +86,12 @@ with tab2:
     st.write(f"vous avez entré la requête solution:{answer}")
 
 
-
 # with tab_x:
-    # 1 st.header("A cat")
-    # 1 st.write("cat")
+# 1 st.header("A cat")
+# 1 st.write("cat")
 
-    # 1 st.header("A cat")
-    # 1 st.image("cheminˇ/chemin2", width= 200)
+# 1 st.header("A cat")
+# 1 st.image("cheminˇ/chemin2", width= 200)
 
-    # 2 input_text= st.text_area(label= "entrez votre input")
-    # 2 st.write(input_text)
+# 2 input_text= st.text_area(label= "entrez votre input")
+# 2 st.write(input_text)
