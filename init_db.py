9596cc908c9e4557cda5b421a4ff388da1b1fd14
root <root@localhost.localdomain>
2025-05-23 02:04:15 +0200

    gitignore et app;y et init_db

---
+++ init_db.py
@@ -0,0 +1,40 @@
+import io
+import pandas as pd
+import duckdb
+
+## création de DB ou de connection
+
+con = duckdb.connect(database='data/exercices_sql_tables.duckdb', read_only=False)
+
+
+# # exo list
+# data = {
+#    "theme": ["cross_joins"],
+#    "exercise_name": ["beverages_and_food"],
+#    "tables": [["beverages", "food_items"]],
+#   "last_reviewed": ["1970-01-01"]
+#}
+
+#memory_state_df = pd.DataFrame(data)
+#con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
+
+# croos_join exo
+
+csv = """
+bevarage, price
+orangejuice, 2.5
+expresso, 2
+tea,3
+"""
+beverages = pd.read_csv(io.StringIO(csv))
+con.execute("CREATE TABLE IF NOT EXISTS beverages AS SELECT * FROM beverages")
+
+
+csv2 = """
+food_item, food_price
+cookie juice, 2.5
+chocolatine, 2
+muffin,3
+"""
+food_items = pd.read_csv(io.StringIO(csv2))
+con.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
