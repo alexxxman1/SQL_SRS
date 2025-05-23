2c20228367391c82b20e7609419ba61b1466fe4b
root <root@localhost.localdomain>
2025-05-24 00:42:17 +0200

    nouvelle apply dynamique

--- app.py
+++ app.py
@@ -3,19 +3,16 @@ import streamlit as st
 import pandas as pd
 import duckdb
 import io
+import ast
 
 # créer une connexion
 
 con = duckdb.connect(database='data/exercices_sql_tables.duckdb', read_only=False)
 
-#answer = """SELECT * FROM beverages CROSS JOIN food_items"""
-# solution = duckdb.query(answer).df()
-
-
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to review",
-        ("cross_joins", "GroupBy", "Windows Functions"),
+        ("cross_joins", "GroupBy", "Window_functions"),
         index=None,
         placeholder="Select a theme ...",
     )
@@ -23,68 +20,52 @@ with st.sidebar:
 
     # la var exercice indexe le theme de chaque exercise dans la table mémory de la db
     # attention '' -> {theme}' car nous ne voulons pas comparer la col theme avec la col cross_join
-    exercise= con.execute(f"select * from memory_state WHERE theme ='{theme}'").df()
+    exercise = con.execute(f"select * from memory_state WHERE theme ='{theme}'").df()
     st.write(exercise)
+
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
 
 st.header("entrer votre code:")
 query = st.text_area(label="votre code sql ici", key="user_input")
 
-'''
-
 if query:
-    result = duckdb.sql(query).df()
+    result = con.execute(query).df()
     st.dataframe(result)
 
+
     try:
-        result = result[solution.columns]
-        st.dataframe(result.compare(solution))
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
     except KeyError as e:
         st.write("some columns are missing")
 
-    n_lines_difference = result.shape[0] - solution.shape[0]
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
     if n_lines_difference != 0:
         st.write(
             f"result has a '{n_lines_difference}' lines difference with the solution"
         )
 
 
+
 tab1, tab2 = st.tabs(["tables", "solution"])
 
 with tab1:
-    # selection et requête
-    # sql_query = st.text_area(label="entrez votre input")
-    # result = duckdb.query(sql_query).df()
-    # st.write(f"vous avez entré la requête:{sql_query}")
-    # st.dataframe(result)
 
-    st.write("beverages")
-    sql_query1 = """SELECT * FROM beverages"""
-    result1 = duckdb.query(sql_query1).df()
-    st.dataframe(result1)
 
-    st.write("food_items")
-    sql_query2 = """SELECT * FROM food_items"""
-    result2 = duckdb.query(sql_query2).df()
-    st.dataframe(result2)
+    exercise_tables= ast.literal_eval(exercise.loc[0, "tables"])
 
-    # AFFICHAGE DU BON RESULTAT
-    st.write("resultat espéré")
-    result3 = duckdb.query(answer).df()
-    st.dataframe(result3)
+    for table in exercise_tables:
+
+        st.write(f"table: {table}")
+        df_table = con.execute(f"select * from '{table}'").df()
+        st.dataframe(df_table)
+
 
 
 with tab2:
     st.header("SOLUTION A LA QUESTION")
-    st.write(f"vous avez entré la requête solution:{answer}")
-
-'''
-
-# with tab_x:
-# 1 st.header("A cat")
-# 1 st.write("cat")
-
-# 1 st.header("A cat")
-# 1 st.image("cheminˇ/chemin2", width= 200)
-
-# 2 input_text= st.text_area(label= "entrez votre input")
-# 2 st.write(input_text)
+    st.write(answer)
--- init_db.py
+++ init_db.py
@@ -9,7 +9,7 @@ con = duckdb.connect(database='data/exercices_sql_tables.duckdb', read_only=Fals
 
 # # exo list
 data = {
-    "theme": ["cross_joins", "window_function"],
+    "theme": ["cross_joins", "GroupBy", "Window_functions"],
     "exercise_name": ["beverages_and_food", "simple_window"],
     "tables": [["beverages", "food_items"], "simple_window"],
     "last_reviewed": ["1970-01-01", "1970-01-01"]
